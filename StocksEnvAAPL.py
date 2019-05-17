95fdf57c168f1a600b3eccae406bcf37acf87dc1
KavitGangar <39722464+KavitGangar@users.noreply.github.com>
2019-05-17 17:30:20 +0530

    Update StocksEnvAAPL.py

--- StocksEnvAAPL.py
+++ StocksEnvAAPL.py
@@ -15,8 +15,8 @@ action_f = open('./numpy.txt', 'a')
 profit_f = open('./profit.txt', 'a')
 
 
-apl_open = d["trv_open"]
-apl_close = d["trv_close"]
+apl_open = d["trv_untrend_open"]
+apl_close = d["trv_untrend_close"]
 #msf_open = d["mo"]
 #msf_close = d["mc"]
 
