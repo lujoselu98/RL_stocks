a6901f67ba4ae8746d4d42c139e58664e90d4f63
KavitGangar <39722464+KavitGangar@users.noreply.github.com>
2019-05-21 01:31:49 +0530

    Update StocksEnvAAPL.py

--- StocksEnvAAPL.py
+++ StocksEnvAAPL.py
@@ -8,7 +8,7 @@ import random
 import matplotlib.pyplot as plt
 
 import pickle
-with open("./aplmsfopenclose.pkl", "rb") as f:
+with open("./mystocktest.pkl", "rb") as f:
     d = pickle.load(f)
     
 action_f = open('./numpy.txt', 'a')
